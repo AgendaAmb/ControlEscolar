fdd97fa869876721457a92b55d6c991a5921a57e
jacobloredo <yeicob_loredo@hotmail.com>
2022-03-09 14:49:15 -0600

    permisos

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,6 @@ use Illuminate\Support\Facades\Route;
 ##COMENTAR ESTE GRUPO DE RUTAS
 
 Route::get('/', [LoginController::class, 'prelogin'])->name('authenticate.prelogin');
-//Route::redirect('/controlescolar', '/');
 
 // Route::prefix('controlescolar')->group(function () {
     # Rutas de autenticacion.
