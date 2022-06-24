da588d72ccecc931795018cd961210fb9ca28ae0
GabrielMontalvo <contact.hhmon@gmail.com>
2022-06-24 18:20:45 -0500

    Agendar sin restricción

--- app/Http/Requests/StoreInterviewRequest.php
+++ app/Http/Requests/StoreInterviewRequest.php
@@ -35,7 +35,8 @@ class StoreInterviewRequest extends FormRequest
             'start_time' => ['required','date_format:H:i'],
             'end_time' => ['required','date_format:H:i','after:start_time'],
             'period_id' => ['required','exists:periods,id','exclude'],
-            'room_id' => ['required','numeric','exists:rooms,id', new RoomRule($this->date, $this->start_time)]
+            // 'room_id' => ['required','numeric','exists:rooms,id', new RoomRule($this->date, $this->start_time)]
+            'room_id' => ['required', 'numeric', 'exists:rooms,id']
         ]; 
     }
 }
