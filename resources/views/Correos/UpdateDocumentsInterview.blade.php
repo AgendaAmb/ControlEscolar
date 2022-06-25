fe60ef5cb0e922e227f980bb5200038650e0ea54
BlurryBunny <CoChiCoriginal@gmail.com>
2022-06-24 21:44:05 -0500

    (fix) send Emails, view documents, and close view for professor V1.3

--- resources/views/Correos/UpdateDocumentsInterview.blade.php
+++ resources/views/Correos/UpdateDocumentsInterview.blade.php
@@ -49,7 +49,7 @@
                     <div class="row mt-2">
                         Dentro de los requisitos, se establece la elaboración de un ensayo académico relacionado con las
                         ciencias ambientales o bien relacionado con tu posible trabajo de tesis, el cual se le solicita
-                        sea ingresado en la plataforma a más tardar el día 19 de junio a las 12:00 h
+                        sea ingresado en la plataforma a más tardar 25 junio a las 11:59 p.m.
                     </div>
                     <div class="row mt-2 mb-2 align-items-center justify-content-center">
                         @component('mail::button',
@@ -64,9 +64,7 @@
                     </div>
                 @elseif ($academic_program['alias'] === 'doctorado')
                     <div class="row mt-2">
-                        Dentro de los requisitos, se establece la elaboración UNA
-                        PRESENTACIÓN Y ENSAYO, las cuales deberán de ser ingresadas a la plataforma a más tardar el día de
-                        junio a las 12:00 hrs.
+                        Dentro de los requisitos, se establece la elaboración UNA PRESENTACIÓN Y ENSAYO, las cuales deberán de ser ingresadas a la plataforma a más tardar el día 25 junio a las 11:59 p.m.
                     </div>
                     <div class="row mt-2 mb-2 align-items-center justify-content-center">
 
