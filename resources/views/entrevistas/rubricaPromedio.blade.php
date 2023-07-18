38a14638d3f42bce3e577139b711233040ce31c9
RicardoRomGar <rikardor18@gmail.com>
2023-07-18 13:53:45 -0600

    Prueba Rubrica

--- resources/views/entrevistas/rubricaPromedio.blade.php
+++ resources/views/entrevistas/rubricaPromedio.blade.php
@@ -17,7 +17,7 @@ const archive_id = @json($id);
 <div class="row mt-5 mb-2">
     <div class="col-12">
         <h1 class="title">Rúbrica promedio de evaluación</h1>
-        <h1 v-if="announcement.type === 'doctorado' " class="sub-title">Doctorado</h1>
+        <h1 v-if="type === 'doctorado' " class="sub-title">Doctorado</h1>
         <h1 v-else class="sub-title">Maestría</h1>
     </div>
     <hr class="col-11 hr">
